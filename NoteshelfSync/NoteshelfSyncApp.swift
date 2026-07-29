e830cfdc301139b1e95bc6a597338987d06739b5
Satvik Reddy Konda <0skonda31@gmail.com>
2026-07-29 15:20:04 +0530

    Initial Commit

---
+++ NoteshelfSync/NoteshelfSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteshelfSyncApp.swift
+//  NoteshelfSync
+//
+//  Created by Satvik  on 2026/07/29.
+//
+
+import SwiftUI
+
+@main
+struct NoteshelfSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
